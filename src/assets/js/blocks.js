0802b8fba89f80ebff740b88d495d631d5c72ef2
Artem Gladkov <art123456789063@gmail.com>
2020-10-16 12:29:57 +0300

    Js реализация блока like

--- src/assets/js/blocks.js
+++ src/assets/js/blocks.js
@@ -2,4 +2,5 @@ import '../../blocks/desktop/dropdown/dropdown.js';
 import '../../blocks/desktop/dropdown/_content/_content_guests.js';
 import '../../blocks/desktop/dropdown/_content/_content_amenities.js'
 import '../../blocks/desktop/toggle/toggle.js'
+import '../../blocks/desktop/like/like.js'
 
---
+++ src/blocks/desktop/like/like.js
@@ -0,0 +1,26 @@
+let likes = $('.like')
+
+$.each(likes, (index, like) => {
+  let heart = $(like).find('.like__heart img'),
+      count = $(like).find('.like__count'),
+      isAdd = false
+  
+  $(like).on('click', () => {
+
+    $(like).toggleClass('active')
+
+    heart.attr('src', function(index, attr){
+      return attr == 'assets/img/heart__border.svg' ? 'assets/img/heart.svg' : 'assets/img/heart__border.svg'
+    })
+
+    if(!isAdd) {
+      count.text(+count.text() + 1)
+      isAdd = true
+    } else{
+      count.text(+count.text() - 1)
+      isAdd = false
+    }
+    
+
+  })
+})
