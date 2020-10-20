caf0449ea6033fc0ca3ddb707ac20e593d84622f
Artem Gladkov <art123456789063@gmail.com>
2020-10-20 10:01:37 +0300

    Подключил slick слайдер для карточки товара, правки header, footer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,9 @@
-import './blocks.js'
+import './blocks.js';
+import '../../libs/slick/slick.js';
+
+$('.card-room__slider').slick({
+  dots: true,
+  prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"></button>',
+  nextArrow: '<button class="slick-next" aria-label="Next" type="button"></button>',
+  rows: 0
+})
