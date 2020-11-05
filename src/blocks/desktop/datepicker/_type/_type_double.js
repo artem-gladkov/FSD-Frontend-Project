93ecb3116ce2b07cb18d0374f0df0a1ac5771d25
Artem Gladkov <art123456789063@gmail.com>
2020-11-05 14:26:36 +0300

    Правки + доделан адаптив сайта

--- src/blocks/desktop/datepicker/_type/_type_double.js
+++ src/blocks/desktop/datepicker/_type/_type_double.js
@@ -13,24 +13,3 @@ $.each(allDoubleDatepickers, function(index, datepicker){
     nextHtml: '<div class="arrow arrow-next"></div>'
   })
 })
-
-// $.each(allDoubleDatepickers, function(index, datepicker){
-//   $(datepicker).find('.datepicker-input__value-to').datepicker({
-//     range: true,
-//     multipleDatesSeparator: '-',
-//     position: "bottom right",
-//     clearButton: true,
-//     onSelect: function (fd, d, picker) { 
-//       $(datepicker).find('.datepicker-input__value-from').val(fd.split("-")[0]);
-//       $(datepicker).find(".datepicker-input__value-to").val(fd.split("-")[1]);
-//     },
-//     prevHtml: '<div class="arrow arrow-prev"></div>',
-//     nextHtml: '<div class="arrow arrow-next"></div>'
-//   })
-// })
-
-// let allButtons = document.querySelectorAll('.datepicker .datepicker--buttons')
-
-// allButtons.forEach(datepickerButtons => {
-//     datepickerButtons.insertAdjacentHTML('beforeend', '<span class="datepicker--button" data-action="hide">Применить</span>')
-// });
